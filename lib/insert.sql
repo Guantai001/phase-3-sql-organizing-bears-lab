9d4189e2676ad2f9b4d366e7627ad4a964f97a04
GUANTAI001 <jpguantai@gmail.com>
2023-02-18 18:37:22 +0300

    completed Assignment

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO Bears(name,age, sex)
+VALUES
+("Mr. Chocolate", 20, "M"),
+("Rowdy", 10, "M"),
+("Tabitha", 6, "F"),
+("Sargeant Brown", 19, "M"),
+("Melissa", 13, "F"),
+("Grinch", 2, "M"),
+("Wendy", 6, "F"),
+(NULL, 1, "M");
